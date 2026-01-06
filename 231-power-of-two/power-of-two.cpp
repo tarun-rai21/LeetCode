8c842d2913515543bf255876b3cce2201f9d1e09
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-01-06 22:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (14.29%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,15 @@
+// Solved by Tarun
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+
+        if(n <= 0 || (n & (n-1)) != 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+};
