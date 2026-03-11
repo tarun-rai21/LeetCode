1498878223a120432b6dea213ea8d8bad0144ea2
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-03-11 16:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.15%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans=0;
+        if(x==0) return 0;
+        if(x==1) return 1;
+        
+        int low = 1;
+        int high = x/2;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(mid == x/mid){
+                ans = mid;
+                break;
+            }
+            if(mid > x/mid){
+                high = mid-1;
+            }
+            if(mid < x/mid){
+                ans = mid;
+                low = mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
