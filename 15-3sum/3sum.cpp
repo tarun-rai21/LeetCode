5155be1ada951fc62b34f9372b910e36f1b9e0ca
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-03-30 06:01:34 +0530

    Time: 50 ms (48.51%) | Memory: 29.2 MB (45.48%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
         vector<vector<int>> result;
 
-        for(int i=0; i<n-2; i++){
-            
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        sort(nums.begin(), nums.end());
 
-            else{
-                int low = i+1;
-                int high = n-1;
-                
-                while(low<high){
-                    if(nums[low] + nums[high] == -nums[i]){
-                        
-                        vector<int>res = {nums[i], nums[low], nums[high]};
-                        result.push_back(res);
-                        
-                        low++;
-                        high--;
+        for (int i = 0; i < n - 2; i++) {
 
-                        while(low < high && nums[low]==nums[low-1]) low++;
-                        while(low < high && nums[high]==nums[high+1]) high--;
-                    }
-                    else if(nums[low] + nums[high] > -nums[i]) high--;
-                    else low++;
+            // skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int low = i + 1;
+            int high = n - 1;
+
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[low], nums[high]});
+
+                    low++;
+                    high--;
+
+                    // skip duplicates for low and high
+                    while (low < high && nums[low] == nums[low - 1]) low++;
+                    while (low < high && nums[high] == nums[high + 1]) high--;
+                }
+                else if (sum < 0) {
+                    low++;
+                }
+                else {
+                    high--;
                 }
             }
         }
+
         return result;
     }
 };
