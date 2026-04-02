7b9e5918e38d60d35947765b50521e7728dc6c5e
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-04-02 15:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.32%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,23 +4,22 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        int high = cols-1;
-        int low = 0;
+        int start = 0;
+        int end = rows*cols - 1;
 
-        int row = 0;
+        int row;
+        int col;
 
-        while (low<=high){
-            if(target > matrix[row][cols-1]){
-                if (row < rows-1) row++;
-                else return 0;
-            }
-            else{
-                int mid = low + (high-low)/2;
-                if(matrix[row][mid]==target) return 1;
-                else if (matrix[row][mid] > target) high = mid-1;
-                else low = mid + 1;
-            }
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            row = mid/cols;
+            col = mid%cols;
+
+            if(matrix[row][col]==target) return 1;
+            else if (matrix[row][col]>target) end = mid-1;
+            else start = mid+1;
         }
+
         return 0;
     }
 };
