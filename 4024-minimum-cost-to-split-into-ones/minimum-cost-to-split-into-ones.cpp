04eff18b16ece0dadbabe17a2dd4c2fd087f3c1e
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-04-09 14:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (29.79%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int minCost(int n) {
-        
-        int cost = 0;
-
-        if(n==1) return cost;
-        else if(n==2) return cost + 1;
-        else{
-            while(n>1){
-                cost += (n-1);
-                n--; 
-            }
-        } 
-        return cost;
+        return n*(n-1)/2;   //sum of digits till n-1
     }
 };
