9acd03bd8afa182868f68c36b7417a462686a415
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-04-10 23:06:32 +0530

    Time: 10 ms (14.79%) | Memory: 10.9 MB (66.93%) - LeetSync

---
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.cpp
@@ -0,0 +1,16 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        while (true) {
+            int num = (rand7() - 1) * 7 + rand7(); // 1..49
+            
+            if (num <= 40) {
+                return (num - 1) % 10 + 1;
+            }
+        }
+    }
+};
