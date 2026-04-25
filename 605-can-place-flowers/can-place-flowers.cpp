35f86bdad310f1605dc2486e09e3d3fb9673266a
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-04-25 16:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (5.96%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -4,26 +4,18 @@ public:
         int n = nums.size();
         int count = 0;
 
-        if(k==0 && n>0) return true;
-        if(n==1 && nums[0]==0 && k<=1) return true;
-        if(n==2){
-            if(nums[0]==0 && nums[1]==0 && k==1) return true;
-            else return false;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                bool left = (i==0 || nums[i-1]==0);
+                bool right = (i==n-1 || nums[i+1]==0);
+
+                if(left && right){
+                    k--;
+                    nums[i]=1;
+                    if(k==0) return true;
+                }
+            }    
         }
-
-        if(nums[0]==0 && nums[1]==0) count++;
-        if(nums[n-1]==0 && nums[n-2]==0) count++;
-
-        for(int i=1; i<n-3;){
-            if(nums[i]==0 && nums[i+1]==0 && nums[i+2]==0){
-                count++;
-                i+=2;
-            }
-            else i++;
-        }
-
-
-        if(k <= count) return true;
-        return false;
+        return k<=0;
     }
 };
