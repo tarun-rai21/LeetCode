9c17eb60a3414eb7c2f5f4886e4ea92d6e9f8e16
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-05-03 13:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (82.14%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -3,34 +3,44 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-
         vector<vector<int>> result;
-        
-        for(int i=0; i<n-3; i++){
 
-            if(i>0 && nums[i]==nums[i-1]) continue; //skip duplicates
+        for(int i = 0; i < n - 3; i++) {
 
-            for(int j=i+1; j<n-2; j++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-                if(j>i+1 && nums[j]==nums[j-1]) continue; //skip duplicates
+            long long min1 = (long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3];
+            if(min1 > target) break;
+
+            long long max1 = (long long)nums[i] + nums[n-1] + nums[n-2] + nums[n-3];
+            if(max1 < target) continue;
+
+            for(int j = i + 1; j < n - 2; j++) {
+
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                long long min2 = (long long)nums[i] + nums[j] + nums[j+1] + nums[j+2];
+                if(min2 > target) break;
+
+                long long max2 = (long long)nums[i] + nums[j] + nums[n-1] + nums[n-2];
+                if(max2 < target) continue;
 
                 int low = j + 1;
                 int high = n - 1;
 
-                while (low < high) {
+                while(low < high) {
                     long long sum = (long long)nums[i] + nums[j] + nums[low] + nums[high];
 
-                    if (sum == target) {
+                    if(sum == target) {
                         result.push_back({nums[i], nums[j], nums[low], nums[high]});
 
                         low++;
                         high--;
 
-                        // skip duplicates for low and high
-                        while (low < high && nums[low] == nums[low - 1]) low++;
-                        while (low < high && nums[high] == nums[high + 1]) high--;
+                        while(low < high && nums[low] == nums[low - 1]) low++;
+                        while(low < high && nums[high] == nums[high + 1]) high--;
                     }
-                    else if (sum < target) {
+                    else if(sum < target) {
                         low++;
                     }
                     else {
