7ad4cbfac84b462db2aee417e2f33dfdd944b2ad
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-05-03 14:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (81.23%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,25 +3,24 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
+        vector<int> result;
 
-        unordered_set<int> mySet;
+        int i = 0; // pointer to nums1
+        int j = 0; // pointer to nums2
+
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        for(int i=0; i<n1; i++){
-            int low = 0;
-            int high = n2-1;
-
-            while(low<=high){
-                int mid = low + (high-low)/2;
-                if(nums1[i] == nums2[mid]){
-                    mySet.insert(nums1[i]);
-                    break;
+        while(i<n1 && j<n2){
+            if(nums1[i] == nums2[j]){
+                if(result.empty() || result.back() != nums1[i]){
+                    result.push_back(nums1[i]);
                 }
-                else if(nums1[i] < nums2[mid]) high = mid-1;
-                else low = mid+1;
+                i++; j++;
             }
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
         }
-        return vector<int>(mySet.begin(), mySet.end());
+        return result;
     }
 };
