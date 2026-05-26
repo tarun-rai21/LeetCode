ef9928c3ac8092389f4bdb533b1cace327984781
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-05-26 22:56:14 +0530

    Time: 21 ms (42.44%) | Memory: 26.1 MB (74.83%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -5,12 +5,12 @@ public:
 
         if (n<3) return false;
 
-        int i=0;
-        
+        int i=0;        
         while(i<n-1){
             if(arr[i]<arr[i+1]) i++;
             else break;
         }
+        
         if(i==0 || i==n-1) return false;
 
         for(int j=i; j<n-1; j++){
