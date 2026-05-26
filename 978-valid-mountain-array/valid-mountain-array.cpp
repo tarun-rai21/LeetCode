ff141edffb82913ad4c5d15440420bb6a97fbffc
Tarun Kumar Rai <tarunrai.2111@gmail.com>
2026-05-26 22:54:52 +0530

    Time: 14 ms (92.50%) | Memory: 26.1 MB (92.73%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n = arr.size();
+
+        if (n<3) return false;
+
+        int i=0;
+        
+        while(i<n-1){
+            if(arr[i]<arr[i+1]) i++;
+            else break;
+        }
+        if(i==0 || i==n-1) return false;
+
+        for(int j=i; j<n-1; j++){
+            if(arr[j]<=arr[j+1]) return false;
+        }
+        return true;
+    }
+};
